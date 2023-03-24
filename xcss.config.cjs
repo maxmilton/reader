7606be1931b9b2cacd2c47f0e581bbae10f3fd3f
Max Milton <max@happyhackers.xyz>
2023-03-24 11:11:12 +1100

    chore: Change file extension of xcss config; js --> cjs

--- xcss.config.cjs
+++ xcss.config.cjs
@@ -1,6 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck - FIXME: Fix ekscss types
-/* eslint-disable @typescript-eslint/no-unsafe-return */
+// x eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// x @ts-nocheck - FIXME: Fix ekscss types
+/* x eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies, strict */
 
 'use strict';
