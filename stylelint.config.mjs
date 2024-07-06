efe5d57ac1e9bbad96a0bc7291d1dca09f58fe67
Max Milton <max@happyhackers.xyz>
2024-07-06 15:09:55 +0900

    chore: Move stylelint config to mjs file

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('stylelint').Config} */
+export default {
+  reportInvalidScopeDisables: true,
+  reportNeedlessDisables: true,
+  extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
+  ignoreFiles: ['*.bak/**', 'dist/**', 'node_modules/**'],
+  rules: {
+    'import-notation': null,
+    'function-no-unknown': null,
+  },
+};
