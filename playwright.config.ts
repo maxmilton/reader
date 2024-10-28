251273bcf915f3a001d610fa1a4d95d2d3e81b53
Max Milton <max@happyhackers.xyz>
2024-10-28 11:34:06 +0900

    chore: Rework playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  testDir: 'test/e2e',
   testMatch: 'test/e2e/**/*.spec.ts',
+  snapshotPathTemplate: 'test/e2e/__snapshots__/{testFilePath}/{arg}{ext}',
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
   use: {
     acceptDownloads: false,
     contextOptions: { strictSelectors: true },
     locale: 'en-US',
     // offline: true,
     timezoneId: 'UTC',
+    trace: 'on-first-retry',
+  },
+  expect: {
+    toHaveScreenshot: {
+      scale: 'device',
+      stylePath: 'test/e2e/screenshot.css',
+    },
   },
 });
