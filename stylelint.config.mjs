5247f7aa9a5a8bcacad6694f2e88a96d09ea8734
Max Milton <max@happyhackers.xyz>
2025-03-02 08:43:36 +0900

    chore: Remove unnecessary Stylelint rule

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -5,7 +5,6 @@ export default {
   extends: ['stylelint-config-standard', '@maxmilton/stylelint-config'],
   ignoreFiles: ['*.bak/**', 'dist/**', 'node_modules/**'],
   rules: {
-    'declaration-property-value-no-unknown': null, // TODO: Enable this rule + implement in postcss-ekscss
     'import-notation': null,
   },
 };
