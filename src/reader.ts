15e88561da68fe0b76bd8990746573dd08e9bf26
Max Milton <max@happyhackers.xyz>
2025-06-24 23:15:13 +0900

    feat: Clean up global types

--- src/reader.ts
+++ src/reader.ts
@@ -1,16 +1,8 @@
 import './css/index.xcss';
 
-import { append, fragment, handleClick, ONCLICK } from 'stage1/fast';
+import { append, fragment, handleClick } from 'stage1/fast';
 import { Footer } from './components/Footer.ts';
 import { Reader } from './components/Reader.ts';
-
-declare global {
-  interface HTMLElement {
-    /** `stage1` synthetic click event handler. */
-    // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-    [ONCLICK]?(event: MouseEvent): false | void | Promise<void>;
-  }
-}
 
 const container = fragment();
 
