e2c4a6e188c7a7388b85ce606572067568d97844
Max Milton <max@happyhackers.xyz>
2025-06-24 22:36:57 +0900

    chore: Fix env types + cleanup

--- modules.d.ts
+++ modules.d.ts
@@ -1,3 +1,9 @@
+declare module 'bun' {
+  interface Env {
+    readonly APP_RELEASE: string;
+  }
+}
+
 declare module '*.css' {
   const content: string;
   export default content;
--- src/components/Footer.ts
+++ src/components/Footer.ts
@@ -6,14 +6,6 @@ import { interpolate } from '../macros.ts' with { type: 'macro' };
 
 export type FooterComponent = HTMLElement;
 
-// const meta = compile(
-//   `
-//     <footer>
-//       © <a href=https://maxmilton.com class="normal muted" target=_blank>Max Milton</a> ・ v${process.env.APP_RELEASE} ・ <a href=https://github.com/maxmilton/reader/issues target=_blank>report bug</a>
-//     </footer>
-//   `,
-//   { keepSpaces: true },
-// );
 const meta = compile(
   // FIXME: This is a convoluted workaround for a bug in the bun macro system,
   // where it crashes when doing string literal template interpolation. See:
@@ -24,7 +16,7 @@ const meta = compile(
         © <a href=https://maxmilton.com class="normal muted" target=_blank>Max Milton</a> ・ v%%1%% ・ <a href=https://github.com/maxmilton/reader/issues target=_blank>report bug</a>
       </footer>
     `,
-    [process.env.APP_RELEASE!],
+    [process.env.APP_RELEASE],
   ),
   { keepSpaces: true },
 );
