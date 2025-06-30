1dd4ff3dd626690ba0e7e6dd611a6b6b66d19698
Max Milton <max@happyhackers.xyz>
2025-06-30 21:27:30 +0900

    feat: Update health script to use new `bugbox/micro` client

--- src/health.ts
+++ src/health.ts
@@ -1,15 +1,14 @@
 /**
  * Tracks JavaScript exceptions using the BugBox client.
  *
- * @fileoverview Chrome Extensions v3 disallow remote code execution,
- * so loading BugBox from a CDN isn't an option. Using a local version
- * is both faster and compliant. Since we're targeting modern browsers,
- * the lite client is sufficient.
+ * @fileoverview Chrome extensions v3 disallows remote code execution, so
+ * loading BugBox from a CDN isn't an option. Using a local version is both
+ * faster and compliant. Since we're targeting modern browsers, the micro
+ * client is sufficient.
  */
 
-// TODO: Use bugbox/lite client once available.
-import * as bugbox from 'bugbox';
+import { meta } from "bugbox/micro";
 
 chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
-  bugbox.meta.url = tab.url;
+  meta.url = tab.url;
 });
