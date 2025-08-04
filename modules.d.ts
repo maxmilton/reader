855aaef0ceb8bee51f9f8e59e926bbfbcf8541a1
Max Milton <max@happyhackers.xyz>
2025-08-04 20:09:36 +0900

    chore: Clean up unused module declarations

--- modules.d.ts
+++ modules.d.ts
@@ -4,16 +4,6 @@ declare module "bun" {
   }
 }
 
-declare module "*.css" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.svg" {
-  const content: string;
-  export default content;
-}
-
 declare module "*.xcss" {
   const content: string;
   export default content;
