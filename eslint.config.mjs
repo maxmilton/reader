02a9d5f56d121f722427703d12b53ceb4f6eb791
Max Milton <max@2okki.com>
2025-10-31 11:16:24 +0900

    chore: Rework eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,19 @@
 import js from "@eslint/js";
 import mm from "@maxmilton/eslint-config";
 import unicorn from "eslint-plugin-unicorn";
+import { defineConfig } from "eslint/config";
 import ts from "typescript-eslint";
 
-const OFF = 0;
-const ERROR = 2;
-
-export default ts.config(
+export default defineConfig(
   js.configs.recommended,
   ts.configs.strictTypeChecked,
   ts.configs.stylisticTypeChecked,
+  // @ts-expect-error - broken upstream types
   unicorn.configs.recommended,
   mm.configs.recommended,
   {
     linterOptions: {
-      reportUnusedDisableDirectives: ERROR,
+      reportUnusedDisableDirectives: "error",
     },
     languageOptions: {
       parserOptions: {
@@ -23,23 +22,13 @@ export default ts.config(
       },
     },
     rules: {
-      quotes: [ERROR, "double", { avoidEscape: true }],
-
-      // Bad browser support
-      "unicorn/prefer-at": OFF,
-      // Prefer to clearly separate Bun and DOM
-      "unicorn/prefer-global-this": OFF,
-
-      /* Performance and byte savings */
-      // byte savings
-      "no-plusplus": OFF,
-      // byte savings (minification doesn't automatically remove)
-      "unicorn/switch-case-braces": [ERROR, "avoid"],
-
-      /* stage1 */
-      "unicorn/prefer-add-event-listener": OFF,
-      "unicorn/prefer-dom-node-append": OFF,
-      "unicorn/prefer-query-selector": OFF,
+      "no-plusplus": "off", // byte savings
+      "unicorn/prefer-add-event-listener": "off", // stage1
+      "unicorn/prefer-at": "off", // bad browser support
+      "unicorn/prefer-dom-node-append": "off", // stage1
+      "unicorn/prefer-global-this": "off", // clearly separate Bun and DOM
+      "unicorn/prefer-query-selector": "off", // stage1
+      "unicorn/switch-case-braces": ["error", "avoid"], // byte savings (minification doesn't automatically remove)
     },
   },
   { ignores: ["**/*.bak", "coverage", "dist"] },
